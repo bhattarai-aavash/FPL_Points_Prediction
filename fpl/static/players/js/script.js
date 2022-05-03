3f3991dd8ebe617531ee176a55adf95c90bbe34e
Anup <anupbasnet057@gmail.com>
2022-05-03 14:42:40 +0545

    addded fixtures js

--- fpl/static/players/js/script.js
+++ fpl/static/players/js/script.js
@@ -49,6 +49,108 @@ allTeams.forEach(team =>{
 
 
 
+// ------------Fixtures Page-------------
+let currentScrollPosition = 0;
+let scrollAmount = 650;
+
+const sCont = document.querySelector('.gameweeks');
+const hScroll =  document.querySelector('.match-week-scroller');
+const btnScrollLeft = document.querySelector('#btn-scroll-left');
+const btnScrollRight = document.querySelector('#btn-scroll-right');
+
+btnScrollLeft.style.opacity = '0';
+
+let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+
+function scrollHorizontally(val) {
+    currentScrollPosition += (val * scrollAmount);
+
+    if(currentScrollPosition >= 0){
+        currentScrollPosition = 0
+        btnScrollLeft.style.opacity = '0';
+    }else{
+        btnScrollLeft.style.opacity = '1';
+    }
+
+    if(currentScrollPosition <= maxScroll){
+        currentScrollPosition = maxScroll;
+        btnScrollRight.style.opacity = '0';
+    }else{
+        btnScrollRight.style.opacity = '1';
+    }
+
+    sCont.style.left = currentScrollPosition + 'px';
+}
+
+
+// ----------show-hide-fixtures--------------
+let currentGameweek = 34;
+let currentGameweekBtn = document.querySelector(`#gw-${currentGameweek}`)
+let gameweekScrollBtns = document.querySelectorAll('.gw-num');
+let gameweekFixturesInfo = document.querySelectorAll('.gw');
+let clickedWeek;
+// console.log(gameweekFixturesInfo);
+
+// add a unique border to current gamewek scroll btn 
+currentGameweekBtn.style.border = `1px solid var(--bg-accent)`;
+currentGameweekBtn.style.color = 'var(--bg-accent)';
+// and show the current btn near to center
+if(currentGameweek >= 7){
+    sCont.style.left = -currentGameweekBtn.offsetLeft/1.15 + 'px';
+    btnScrollLeft.style.opacity = '1';
+    btnScrollRight.style.opacity = '1';
+}
+if(currentGameweek == 38){
+    sCont.style.left = -currentGameweekBtn.offsetLeft/1.2 + 'px';
+    btnScrollRight.style.opacity = '0';
+}
+
+
+
+// show current week fixtures at beginning
+// document.querySelectorAll(`.gw-${currentGameweek}`).classList.add('gw-show');
+document.querySelectorAll(`.gw-${currentGameweek}`).forEach((match)=>{
+    match.classList.add('gw-show');
+})
+document.getElementById('show-gw-no').textContent = currentGameweek;
+
+
+// show clicked week fixture and hide all others
+gameweekScrollBtns.forEach( (weekBtn) =>{
+    weekBtn.addEventListener('click',function(){
+
+        clickedWeek = this.id;
+        
+        gameweekFixturesInfo.forEach( (gw) =>{
+            gw.classList.remove('gw-show')
+            if(clickedWeek == gw.classList[1]){
+                gw.classList.add('gw-show')
+            }
+        })
+
+        
+        gameweekScrollBtns.forEach( (gwBtn) => {
+            gwBtn.style.border = 'none';
+            gwBtn.style.color = 'initial';
+
+            if(clickedWeek == gwBtn.id){
+                gwBtn.style.border = '1px solid var(--bg-accent)'
+                gwBtn.style.color = 'var(--bg-accent)';
+            }
+        })
+
+        // change week no shown accordingly
+        document.getElementById('show-gw-no').textContent = clickedWeek.split('-')[1];
+        
+    })
+    
+})
+
+
+
+
+
+
 // ----------------Pick Team Page-------------
 let teamId = document.getElementById('team-id').textContent;
 document.getElementById('team-id').style.display = 'none';
@@ -152,6 +254,11 @@ if(location.href == `http://127.0.0.1:8000/players/${teamId}/`){
 
 
 
+
+
+
+
+
 // ----------------Points-Page---------------------
 if(location.href == `http://127.0.0.1:8000/players/team/${teamId}/`){
     let myPlayers = document.getElementsByClassName('player');
@@ -160,4 +267,3 @@ if(location.href == `http://127.0.0.1:8000/players/team/${teamId}/`){
 
 }
 
-console.log('dsfa')
